a4ec1266d0c349979b5a40f5d4fcbb5604788993
Kanak Prajapati <kn436428@dal.ca>
2022-03-14 18:49:50 -0300

    updated outlier.py

--- outlier.py
+++ outlier.py
@@ -1,16 +1,20 @@
 import csv
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
 # Reading CSV file(s)
-with open('data/AAPL.csv', 'r') as AAPL:
-    appleData = list(csv.reader(AAPL, delimiter=';'))
- 
-npAppleData = np.array(appleData)
-print(npAppleData)
+
+df = pd.read_csv(r'data/sp500/csv/AAPL.csv')
+T = df.to_numpy()
+print(T)
+
+
 
 # ALGORITHM 1: Anomaly price and volume finding algorithm.
 # Input : M(i,:), Ms(i,:), prices
 # Output : Anomaly price, Anomaly volume
+
 
 
 # ALGORITHM 2: Anomaly location finding algorithm.
