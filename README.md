090e09c4d8cc1e77e86a5ad6dbd2fbf8bfd2540f
Kanak Prajapati <kn436428@dal.ca>
2022-04-06 19:34:53 -0300

    Documentation Added

--- README.md
+++ README.md
@@ -1,10 +1,97 @@
-# Data Mining to analyze stock market data using Lagged Correlation.
+# Analyzing Stock Market Data using Lagged Correlation Data Mining Algorithm.
 
-## Dataset
+## Project Description
 
-The [Stock Market Data(NASDAQ, NYSE, S&P500)](https://www.kaggle.com/paultimothymooney/stock-market-data) used for this project was from kaggle.com. The dataset contains Date, Volume, High, Low, and Closing Price (for all NASDAQ, S&P500, and NYSE listed companies), which is updated weekly by [Paul Mooney](https://www.kaggle.com/paultimothymooney).
+In this project, I have developed a lagged correlation algorithm for predicting the market direction more accurately when two stocks are strongly correlated to each other with a lag of K number of trading days.
 
-## Research Paper
+- Technology Stack
 
-https://ieeexplore.ieee.org/abstract/document/4783968
+  - [Anaconda](https://docs.anaconda.com/anaconda/install/index.html)
+  - [Jupyter Notebook](https://jupyter.org/install)
+  - [PyCharm IDE](https://www.jetbrains.com/pycharm/)
+  - [Python](https://www.python.org/downloads/)
 
+    - Dependecies Used: pandas, csv, glob, and os
+
+- Challanges
+
+  - The program requires huge computational power to perform analysis on a massive dataset.
+
+  - The testing of dataset is a time consuming process. So, I assembled a manual dataset of Top 10 stocks to perform quick analysis.
+
+## Project Structure
+
+```
++-- main.py
+|-- data
+|   +-- cleanData
+|   +-- forbes2000
+|   +-- nasdaq
+|   +-- nyse
+|   +-- output
+|   +-- top10
++-- Readme.md
++-- Readme.txt
++-- screenshots
++-- A_Data_mining_algorithm_to_analyse_stock_market_data_using_lagged_correlation.pdf
+
+```
+
+![](screenshots/projectStructure.png)
+
+---
+
+## Demo
+
+- User is asked to enter lag number and R number.
+
+  ![](screenshots/programDemo.png)
+
+## Sample Output
+
+- This is the sample correlation output for top 10 stocks.
+
+  ![](screenshots/outputSample.png)
+
+### Reading the Output
+
+- The output of the program is in csv file as "final_output.csv" in data/output directory. The output has 4 columns:
+  - Company A: Stock of a company.
+  - Company B: Stocks being correlated to Column A.
+  - Correlation: The value of correlation. If the value is near to +1, it has a positive correlation. If the value is near to -1, it has negative correlation. If the value is 0, correaltion does not exists between Company A and Company B.
+  - Trend: Based on Correlation column. If greater than 0, it will show Up trend. If less than 0 it will show Down trend.
+
+## Using the Project
+
+1. Go to the source folder and run the following command:
+
+   ```bash
+      python main.py
+   ```
+
+2. User will be asked to enter the lag (Number of days) and R (Correlation). Sample input can be 5 for lag number and 0.3 for R number.
+
+   ```bash
+      Enter lag number: 5
+      Enter R number: 0.3
+   ```
+
+3. The output will be saved in data/output directory. To check the output run the following commands in terminal:
+
+   ```bash
+        cd data/output
+   ```
+
+```bash
+    vim final_output.csv
+```
+
+---
+
+## References
+
+- Dataset - The [Stock Market Data(NASDAQ, NYSE, S&P500)](https://www.kaggle.com/paultimothymooney/stock-market-data) used for this project was from kaggle.com. The dataset contains Date, Volume, High, Low, and Closing Price (for all NASDAQ, S&P500, and NYSE listed companies), which is updated weekly by [Paul Mooney](https://www.kaggle.com/paultimothymooney).
+
+- Research Paper - The program implements the lagged correlation algorithm based on
+  [A Data mining algorithm to analyse stock market data
+  using lagged correlation](https://ieeexplore.ieee.org/abstract/document/4783968) research paper.
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
+'''
+This is the main (only) class for the program. This program analyzes stock market data using lagged correlation algorithm.
+The program implements the lagged correlation algorithm as per the research paper. (https://ieeexplore.ieee.org/abstract/document/4783968)
+'''
+
+# Importing necessary dependencies.
+
+# Reference for Pandas Documentation - https://pandas.pydata.org/
 import pandas as pd
 import os
 from glob import glob
 import csv
 
-# Data Cleaning
+'''
+This method gets the dataset to perform correlation. The cleaned dataset is stored in the cleanData directory. 
+
+NOTE FOR DATASET TESTING:
+    Testing the big data (Forbes 2000, Nasdaq, and NYSE) will take hours to compute as it checks for all the possible correlations between those stocks.
+    So, I have created a manual dataset of Top 10 stocks for a quick test, which finds correlation between the top 10 stocks.
+    If you want to test any big datasets just uncomment the line below "To test data for " and comment the line below top 10 stocks.
+'''
 def cleanData():
 
+    # To test data for forbes 2000 stocks
+    # os.chdir("data/forbes2000")
 
+    # To test data for nasdaq stocks
+    # os.chdir("data/nasdaq")
+
+    # To test data for nyse stocks
+    # os.chdir("data/nyse")
+
+    # To test data for top 10 stocks
     os.chdir("data/top10")
-    stocks = glob("*.csv")
-    for i in range(len(stocks) - 1):
 
+    # Getting all the csv files
+    stocks = glob("*.csv")
+
+    for i in range(len(stocks) - 1):
+        
         if i == 0:
             file1_Data = pd.read_csv(stocks[i])
         else:
@@ -18,38 +45,62 @@ def cleanData():
 
         file2_Data = pd.read_csv(stocks[i + 1])
 
-        # using merge function by setting how='inner'
+        # Using the merge function by setting how='inner' to get the inner data from csv files.
         output = pd.merge(file1_Data, file2_Data, on='Date', how='inner')
-
+        
     df = pd.DataFrame(output["Date"])
 
     for i in range(len(stocks)):
+        # For forbes 2000 directory
+        # os.chdir("../forbes2000")
+
+        # For nasdaq directory
+        # os.chdir("../nasdaq")
+
+        # For nyse directory
+        # os.chdir("../nyse")
+
+        # For top 10 directory
         os.chdir("../top10")
+        
         file_data = pd.read_csv(stocks[i])
         final_output = pd.merge(file_data, df, on="Date", how="inner")
         os.chdir("../cleanData")
         st = 'Output_' + stocks[i]
         final_output.to_csv(st, index=False)
+        print("Data has been cleaned for: ", stocks[i])
 
-    print("Data has been cleaned and stored in the cleanData directory")
+    print("\n Data has been cleaned and stored in data/cleanData directory")
 
+'''
+This method performs correlation between the stocks. It asks user to enter the K (Lag) and R (Correlation). 
+The algorithm to perform correlation is adapted from the following paper:
+URL:    https://ieeexplore.ieee.org/abstract/document/4783968
+DOI:    10.1109/ICIAFS.2008.4783968
+
+REFERENCE: 
+C. Fonseka and L. Liyanage, "A Data mining algorithm to analyse stock market data using lagged correlation," 2008 4th International Conference on Information and Automation for Sustainability, 2008, pp. 163-166, doi: 10.1109/ICIAFS.2008.4783968.    
+
+'''
 
 def corelation():
-    # User Input
-    print("Finding correlation between the stocks")
-
+    # Getting User Input.
     K = int(input("Enter lag number: "))
     R = float(input("Enter R number: "))
 
-    # Algorithm
     m = 0
-    header = ['Company A', 'Company B', 'Co-relation','Trend']
+    # Header for out file (csv).
+    header = ['Company A', 'Company B', 'Co-relation','Trend']  
 
+    # Writing the output file.
     os.chdir("../output")
     f = open('final_output.csv', 'w+')
 
+    # Writing the files in cleanData directory.
     os.chdir("../cleanData")
     stocks = glob("*.csv")
+
+    # Looping through the stocks and comparing the csv files data.
     for i in range(len(stocks)):
         for j in range(i + 1, len(stocks) - 1):
             data1 = pd.read_csv(stocks[i])
@@ -67,12 +118,13 @@ def corelation():
                 data2 = data2[temp:temp+k]
                 data1 = data1.squeeze()
                 data2 = data2.squeeze()
+
+                # REFERENCE : https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.corr.html
                 r = data1.corr(data2, method='pearson')
                 temp = temp + k
                 if r > R or r < -R:
                     row = []
                     m = m + 1
-                    # splitChar = split('.')[0].split('_')[1]
                     row.append(stocks[i].split('.')[0].split('_')[1])
                     row.append(stocks[j].split('.')[0].split('_')[1])
                     row.append(r)
@@ -88,10 +140,13 @@ def corelation():
                         writer.writerow(header)
 
                     writer.writerow(row)
+                    print("Finding correlation between", stocks[i], " ",stocks[j], "has been completed" )
     f.close()
 
 
 if __name__ == '__main__':
+    # Calling the cleanData function to clean the data.
     cleanData()
+    # Calling the correlation function to perform correlation using the lagged correlation algorithm.
     corelation()
-    print("The result is printed as file_output.csv in the output folder.")
+    print("\n \nThe result is printed as file_output.csv in the data/output directory.")
